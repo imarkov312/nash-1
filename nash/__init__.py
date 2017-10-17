aed5d06a0d90375acc22a6922a8b88743fadfe38
Alexandr <asmv8@ya.ru>
2017-10-17 15:35:13 +0300

    downloaded package

---
+++ nash/__init__.py
@@ -0,0 +1,20 @@
+def nash_equlibrium(a):
+    m, n = a.shape 
+    
+    #здесь минимизируем со знаком > (lines)
+    c = np.ones((m,))
+    b_ub = np.ones((n,))
+    b_ub = -1 * b_ub
+    a_new = -1 * np.transpose(a)
+    res1 = linprog(c, A_ub = a_new, b_ub = b_ub)
+    v, p = 1.0/res1.fun, res1.x
+    p = v * p
+    
+    #максимизируем со знаком < (columns)
+    c2 = np.ones((n,))
+    c2 = -1 * c2
+    b_ub2 = np.ones((m,))
+    res2 = linprog(c2, A_ub = a, b_ub = b_ub2)
+    q = res2.x
+    q = v * q
+    return v, p, q
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nash',
+      version='0.1',
+      description='Nash_equilibrium',
+      url='https://github.com/smvlx/nash',
+      author='Alexandr Smirnov',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['nash'],
+      zip_safe=False)
